2a6ea92cfce5724ffa021f183382a8171c1bf782
KingsleyOC <kingsleyokechukwu890@gmail.com>
2024-05-27 14:59:00 -0700

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createreportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
